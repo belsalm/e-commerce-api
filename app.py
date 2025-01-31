4e28e1a1e36dd5845965b5c754d5583080f49cd2
bel-salm <almeidasalesisabel@gmail.com>
2025-01-31 07:43:28 -0300

    include new endpoints

--- app.py
+++ app.py
@@ -34,20 +34,51 @@ def delete_product(product_id):
         return jsonify({"message": "Product deleted successfully!"})
     return jsonify({"message": "Product not found"}), 404
 
-    
-# @app.route('/products/delete/<int:product_id>', methods=["DELETE"])
-# def delete_product(product_id):
-#     # Recuperar o produto da base de dados
-#     # Verificar se o produto existe
-#     # Se existe, apagar da base de dados
-#     # Se não existe, retornar 404 not found
-#     product = Product.query.get(product_id)
-#     if product:
-#         db.session.delete(product)
-#         db.session.commit()
-#         return jsonify({"messsage": "Product deleted successfully"})
-#     return jsonify({"message": "Product not found"}), 404
 
+@app.route('/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404 
+
+@app.route("/products/update/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+        
+    if 'price' in data:
+        product.price = data['price']
+    
+    if 'description' in data: 
+        product.description = data['description']
+        
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"})
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
+        product_list.append(product_data)
+    
+    return jsonify(product_list)
 
 # Definir uma rota raiz (Página inicial) e a função que será executada ao  requisitar
 @app.route('/test', methods=["GET"]) 
